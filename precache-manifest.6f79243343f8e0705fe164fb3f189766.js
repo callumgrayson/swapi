435bdedc8f14b503e02da4d3acedef3697523e3c
callumgrayson <callum@musiio.com>
2019-08-29 18:21:37 +0800

    Updates

---
+++ precache-manifest.6f79243343f8e0705fe164fb3f189766.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57562d95a5a2727cbc1cf5e20d76bd23",
+    "url": "/swapi/index.html"
+  },
+  {
+    "revision": "ae790df582918ba2166c",
+    "url": "/swapi/static/css/main.6aeee6ab.chunk.css"
+  },
+  {
+    "revision": "6fcdbc4ec0888f2c1e80",
+    "url": "/swapi/static/js/2.f989f947.chunk.js"
+  },
+  {
+    "revision": "ae790df582918ba2166c",
+    "url": "/swapi/static/js/main.8612b4b5.chunk.js"
+  },
+  {
+    "revision": "a2e45b990c1a88e1ac3b",
+    "url": "/swapi/static/js/runtime~main.1176b128.js"
+  },
+  {
+    "revision": "0a12a758def6aa207e9ddbc3cade937e",
+    "url": "/swapi/static/media/Star_Wars_Logo.0a12a758.svg"
+  },
+  {
+    "revision": "02916d98e96683bef6b6d9dc7217a67d",
+    "url": "/swapi/static/media/lightsaber_blue2.02916d98.png"
+  }
+]);
